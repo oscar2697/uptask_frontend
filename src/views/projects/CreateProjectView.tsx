22ca2081f311956bf3d71b6c7d7fbc9dd11245e1
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-08-02 00:23:40 -0500

    "Marked subproject as dirty"

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import ProjectForm from '@/components/projects/ProjectForm'
 import { ProjectFormData } from '@/types/index'
 import { createProject } from '@/services/ProjectApi'
 
 const CreateProjectView = () => {
+    const navigate = useNavigate()
     const initialValues : ProjectFormData = {
         projectName: '',
         clientName: '',
@@ -13,8 +14,9 @@ const CreateProjectView = () => {
 
     const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
 
-    const handleForm = (data: ProjectFormData) => {
-        createProject(data)
+    const handleForm = async (data: ProjectFormData) => {
+        await createProject(data)
+        navigate('/')
     }
 
     return (
