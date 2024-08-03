d98b6589bea6dd62537494bdd70272d904e04add
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-08-03 01:22:28 -0500

    "Updated subproject commit to indicate dirty state"

---
+++ src/components/tasks/TaskList.tsx
@@ -0,0 +1,66 @@
+import { Task } from "@/types/index";
+import TaskCard from "./TaskCard";
+
+type TaskListProps = {
+    task: Task[];
+}
+
+type GroupedTasks = {
+    [key: string]: Task[]
+}
+
+const initialStatusGroups: GroupedTasks = {
+    pending: [],
+    onHold: [],
+    inProgress: [],
+    underReview: [],
+    completed: [],
+}
+
+const StatusTranslations : {[key: string] : string} = {
+    pending: 'Pending',
+    onHold: 'On Hold',
+    inProgress: 'In Progress',
+    underReview: 'Under Review',
+    completed: 'Completed',
+}
+
+const StatusStyle : {[key: string] : string} = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-cyan-500',
+    inProgress: 'border-t-yellow-500',
+    underReview: 'border-t-fuchsia-500',
+    completed: 'border-t-emerald-500',
+}
+
+const TaskList = ({ task }: TaskListProps) => {
+    const groupedTasks = task.reduce((acc, task) => {
+        let currentGroup = acc[task.status] ? [...acc[task.status]] : []
+        currentGroup = [...currentGroup, task]
+        return { ...acc, [task.status]: currentGroup }
+    }, initialStatusGroups)
+
+    return (
+        <>
+            <h2 className="text-5xl font-black my-10">Tasks</h2>
+
+            <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+                {Object.entries(groupedTasks).map(([status, tasks]) => (
+                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                        <h3 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${StatusStyle[status]}`}>{StatusTranslations[status]} </h3>
+
+                        <ul className='mt-5 space-y-5'>
+                            {tasks.length === 0 ? (
+                                <li className="text-gray-500 text-center pt-3">No Tasks</li>
+                            ) : (
+                                tasks.map(task => <TaskCard key={task._id} task={task} />)
+                            )}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default TaskList
--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/layouts/AppLayout'
 import DashboardView from '@/views/DashboardView'
 import CreateProjectView from '@/views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
+import ProjectsDetails from './views/projects/ProjectsDetails'
 
 
 const Router = () => {
@@ -12,6 +13,7 @@ const Router = () => {
                 <Route element={<AppLayout/>}>
                     <Route path='/' element={<DashboardView/>} index />
                     <Route path='/projects/create' element={<CreateProjectView/>}/>
+                    <Route path='/projects/:projectId' element={<ProjectsDetails/>}/>
                     <Route path='/projects/:projectId/edit' element={<EditProjectView/>}/>
                 </Route>
             </Routes>
